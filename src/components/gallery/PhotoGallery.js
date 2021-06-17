9a1407727fe9453a647580f5bde14e67846eccc1
hugary1995 <hugary1995@gmail.com>
2021-06-16 23:47:11 -0400

    Add gallery lightbox

--- src/components/gallery/PhotoGallery.js
+++ src/components/gallery/PhotoGallery.js
@@ -1,14 +1,55 @@
-import React, {
-  Component
-} from "react";
+import React, { Component } from "react";
 import Gallery from "react-photo-gallery";
-import {
-  photos
-} from './photos.js'
+import { photos } from "./photos.js";
+import { Modal, Button } from "react-materialize";
 
 class PhotoGallery extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentImage: 0,
+      viewerIsOpen: false,
+    };
+  }
+
+  openLightbox = (event, { photo, index }) => {
+    this.setState({ currentImage: index, viewerIsOpen: true });
+  };
+
+  closeLightbox = () => {
+    this.setState({ currentImage: 0, viewerIsOpen: false });
+  };
+
   render() {
-    return (<Gallery photos={photos}/>);
+    return (
+      <div>
+        <Gallery photos={photos} onClick={this.openLightbox} />
+        <Modal
+          actions={[
+            <Button
+              flat
+              modal="close"
+              node="button"
+              waves="green"
+              onClick={this.closeLightbox}
+            >
+              Close
+            </Button>,
+          ]}
+          open={this.state.viewerIsOpen}
+          options={{
+            dismissible: false,
+            preventScrolling: true,
+          }}
+        >
+          <img
+            alt=""
+            src={photos[this.state.currentImage].src}
+            style={{ maxWidth: "100%" }}
+          />
+        </Modal>
+      </div>
+    );
   }
 }
 
