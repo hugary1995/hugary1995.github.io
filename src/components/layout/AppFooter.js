3360199cd78c11cbf1f5c8d52c50ef11e83116a4
hugary1995 <hugary1995@gmail.com>
2021-06-20 12:43:19 -0400

    Update copyright year

--- src/components/layout/AppFooter.js
+++ src/components/layout/AppFooter.js
@@ -1,5 +1,5 @@
-import React, {Component} from "react";
-import {Footer} from "react-materialize";
+import React, { Component } from "react";
+import { Footer } from "react-materialize";
 
 function Contact(props) {
   return (
@@ -19,13 +19,21 @@ function Contact(props) {
   );
 }
 
+function CurrentYear() {
+  return new Date().getFullYear();
+}
+
 class AppFooter extends Component {
   render() {
     return (
       <Footer
-        copyrights="&copy; 2020 Tianchen (Gary) Hu All Rights Reserved"
+        copyrights={
+          <div>
+            &copy; <CurrentYear /> Tianchen (Gary) Hu All Rights Reserved{" "}
+          </div>
+        }
         className="blue darken-4"
-        style={{minHeight: "1em", marginTop: "2em"}}
+        style={{ minHeight: "1em", marginTop: "2em" }}
       >
         <div className="row">
           <div className="col s4">
